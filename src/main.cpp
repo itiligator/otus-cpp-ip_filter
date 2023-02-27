ab8cb73da6b906fed36aa2152b7f0a58124a5173
Andrei Itiligator <a.e.itiligator@gmail.com>
2023-02-27 11:42:54 +0700

    initial commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,3 @@
-#include "../include/metainfo.h"
-
-#include <iostream>
-
 int main(int, char **) {
-	std::cout << "Version: " << patch_number() << std::endl;
-	std::cout << "Hello, world!" << std::endl;
 	return 0;
 }
